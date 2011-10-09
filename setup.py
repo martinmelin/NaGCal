d0fd25305eb2491f76b5a504f936df009d38100f
Martin Melin <git@martinmelin.com>
2011-10-09 16:40:15 +0200

    update filename for config and bump version

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ distribute_setup.use_setuptools()
 from setuptools import setup
 
 setup(name='NaGCal',
-        version='0.1.0',
+        version='0.1.1',
         description='On Call Schedules in Google Calendar for Nagios',
         author='Martin Melin',
         author_email='nagcal@martinmelin.se',
         packages=['nagcal'],
         scripts=['scripts/mail-to-oncall', 'scripts/nagcal'],
-        data_files=[('config', ['cfg/nagcal.cfg'])],
+        data_files=[('config', ['cfg/nagcal.cfg.example'])],
         install_requires=[
             'gdata',
             'iso8601',
