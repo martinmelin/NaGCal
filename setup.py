34a5a50fa75c1bcd089f8410c26392161bbda673
Martin Melin <martin.melin@blocket.se>
2012-01-04 13:14:35 +0100

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ distribute_setup.use_setuptools()
 from setuptools import setup
 
 setup(name='NaGCal',
-        version='0.1.2',
+        version='0.1.3',
         description='On Call Schedules in Google Calendar for Nagios',
         author='Martin Melin',
         author_email='nagcal@martinmelin.se',
